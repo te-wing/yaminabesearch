a16a896449a07ed4ae4a32e805123dc420059677
wing-0902 <tsubasa@me.wing.osaka>
2025-08-10 11:08:44 +0900

    検索エンジンを追加

--- src/data/searchEngines.ts
+++ src/data/searchEngines.ts
@@ -3,10 +3,20 @@ export interface SearchEngine {
   url: string;
 }
 
-// 検索エンジンの一覧です。ここに追加・削除・変更ができます。
+// 検索エンジン一覧
 export const searchEngines: SearchEngine[] = [
   { name: 'Google', url: 'https://www.google.com/search?q=' },
   { name: 'Bing', url: 'https://www.bing.com/search?q=' },
   { name: 'DuckDuckGo', url: 'https://duckduckgo.com/?q=' },
   { name: 'Yahoo!', url: 'https://search.yahoo.com/search?p=' },
+  { name: 'Ecosia', url: 'https://www.ecosia.org/search?q=' },
+  { name: 'Startpage', url: 'https://www.startpage.com/sp/search?q=' },
+  { name: 'Brave Search', url: 'https://search.brave.com/search?q=' },
+  { name: 'Wolfram|Alpha', url: 'https://www.wolframalpha.com/input?i='},
+  { name: 'Yahoo! JAPAN', url: 'https://search.yahoo.co.jp/search?p='},
+  { name: 'Baidu', url:'https://www.baidu.com/s?wd='},
+  { name: 'Naver', url:'https://search.naver.com/search.naver?query='},
+  { name: 'Coc Coc', url:'https://coccoc.com/search?query='},
+  { name: 'Yandex', url:'https://yandex.com/search/?text='},
+  { name: 'Mojeek', url:'https://www.mojeek.com/search?q='},
 ];
