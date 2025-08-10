60f92a87317980a265ce255963d1e2e8329423f8
wing-0902 <tsubasa@me.wing.osaka>
2025-08-10 00:45:03 +0000

    おっけぇ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+'use client'; // App Routerのクライアントコンポーネント
 
 import styles from './page.module.scss';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation'; // ここを修正
 import type { FormEvent, ChangeEvent } from 'react';
 
 export default function Home() {
-  const [searchWord, setSearchWord] = useState<string>(''); // string型を指定
+  const [searchWord, setSearchWord] = useState<string>('');
   const router = useRouter();
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -14,13 +14,12 @@ export default function Home() {
   };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // デフォルトのフォーム送信をキャンセル
+    event.preventDefault();
     if (searchWord) {
       const url = `/search?word=${encodeURIComponent(searchWord)}`;
       router.push(url);
     }
   };
-
 
   return (
     <>
@@ -28,13 +27,20 @@ export default function Home() {
         <h1 className={styles.h1}>闇鍋検索</h1>
       </header>
       <form onSubmit={handleSubmit} id='searchForm'>
-        <input onChange={handleInputChange} type='text' value={searchWord} id='searchInput' name='word' placeholder='なんか入力して検索'></input>
+        <input
+          onChange={handleInputChange}
+          type='text'
+          value={searchWord}
+          id='searchInput'
+          name='word'
+          placeholder='なんか入力して検索'
+        />
         <button type='submit'>
           検索！
         </button>
       </form>
       <footer>
-
+        {/* フッターの内容 */}
       </footer>
     </>
   );
