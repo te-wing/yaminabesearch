fadedbf3765e461d6603025dbdda67c7b3a8edf5
wing-0902 <tsubasa@me.wing.osaka>
2025-08-10 01:14:34 +0000

    どうや？

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,27 +1,19 @@
 import { redirect } from 'next/navigation';
 import { searchEngines } from '@/data/searchEngines';
+import { PageProps } from 'next';
 
-export default function SearchPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+export default function SearchPage({ searchParams }:PageProps) {
 
-  // URLからクエリパラメータ（'q'）を取得します。
   const query = searchParams.q;
 
-  // クエリがない場合は、トップページにリダイレクトします。
   if (!query || typeof query !== 'string') {
     redirect('/');
   }
 
-  // リストからランダムに検索エンジンを一つ選びます。
   const randomIndex = Math.floor(Math.random() * searchEngines.length);
   const selectedEngine = searchEngines[randomIndex];
 
-  // 選んだ検索エンジンのURLにクエリを付加します。
   const redirectUrl = `${selectedEngine.url}${encodeURIComponent(query)}`;
 
-  // 指定されたURLへリダイレクトします。
   redirect(redirectUrl);
 }
